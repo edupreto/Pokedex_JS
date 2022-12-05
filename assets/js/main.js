ad7af3b5f7795c3995b1287408ff746a7e26d944
edupreto <avelino.edu@gmail.com>
2022-12-05 20:25:26 -0300

    Inicio Implementação detail

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,39 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
+// Limite de pokemons da primeira geração
 const maxRecords = 151
+// Controle de paginação
 const limit = 10
 let offset = 0;
 
+function loadPokemonItens(offset, limit) {
+    // pegando lista de pokemons - nome e url
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) =>{
+    //percorrendo a lista e retornando outra em html
+    // transformando o array em uma só variável com o join para concatenar o html e renderizar de uma vez no DOM
+    const newHtml = pokemons.map(convertPokemonToLi).join("");
+    pokemonList.innerHTML += newHtml;
+    console.log(pokemons);
+    })
+    .catch((error) => console.error(error))
+}
+
+loadPokemonItens();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Convertendo lista de pokemons json para html
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
@@ -22,6 +51,13 @@ function convertPokemonToLi(pokemon) {
         </li>
     `
 }
+
+// Convertendo a lista de detalhes  
+
+
+
+
+
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
